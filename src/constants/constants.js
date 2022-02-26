18636df0c462b48fb13dfcb1794e0dcf8c1f066a
emadmohamed95 <emadmohamed95@gmail.com>
2022-02-26 14:38:52 +0200

    added icons and ios archive settings

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 // export const URL = process.env.REACT_APP_HOST_IP_ADDRESS?process.env.REACT_APP_HOST_IP_ADDRESS:"https://odd-deer-75.loca.lt";
-// export const URL = process.env.REACT_APP_HOST_IP_ADDRESS?process.env.REACT_APP_HOST_IP_ADDRESS:"https://kdm.avvision.net";
-export const URL = process.env.REACT_APP_HOST_IP_ADDRESS?process.env.REACT_APP_HOST_IP_ADDRESS:"http://localhost:8080";
+export const URL = process.env.REACT_APP_HOST_IP_ADDRESS?process.env.REACT_APP_HOST_IP_ADDRESS:"https://kdm.avvision.net";
+// export const URL = process.env.REACT_APP_HOST_IP_ADDRESS?process.env.REACT_APP_HOST_IP_ADDRESS:"http://localhost:8080";
